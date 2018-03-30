b008ffddefb1d86be352da93d1ea45bacb616767
Sandeep <sandeeptile@gmail.com>
2018-03-31 03:52:17 +0530

    logout button added in menu

--- app/src/main/java/com/example/sandy/androidchatapp/MainActivity.java
+++ app/src/main/java/com/example/sandy/androidchatapp/MainActivity.java
@@ -3,11 +3,16 @@ package com.example.sandy.androidchatapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
 
     private FirebaseAuth mAuth;
 
@@ -17,6 +22,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mAuth = FirebaseAuth.getInstance();
+
+        mToolbar=(Toolbar)findViewById(R.id.register_toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("Android App");
     }
 
     @Override
@@ -27,11 +36,39 @@ public class MainActivity extends AppCompatActivity {
 
         if(currentUser == null){
 
-            Intent startIntent=new Intent(MainActivity.this,StartActivity.class);
-            startActivity(startIntent);
-            finish();
+            sendToStart();
 
         }
 
+    }
+
+    private void sendToStart() {
+
+        Intent startIntent=new Intent(MainActivity.this,StartActivity.class);
+        startActivity(startIntent);
+        finish();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+         super.onCreateOptionsMenu(menu);
+
+         getMenuInflater().inflate(R.menu.main_menu, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+         super.onOptionsItemSelected(item);
+
+         if (item.getItemId()==R.id.main_logout_btn){
+
+             FirebaseAuth.getInstance().signOut();
+             sendToStart();
+
+         }
+
+        return true;
     }
 }
--- app/src/main/java/com/example/sandy/androidchatapp/RegisterActivity.java
+++ app/src/main/java/com/example/sandy/androidchatapp/RegisterActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -21,6 +22,8 @@ public class RegisterActivity extends AppCompatActivity {
     private EditText mPassword;
     private Button mCreateBtn;
 
+    private Toolbar mToolbar;
+
     private FirebaseAuth mAuth;
 
     @Override
@@ -28,6 +31,14 @@ public class RegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        //Toolbar set
+         mToolbar=(Toolbar)findViewById(R.id.register_toolbar);
+         setSupportActionBar(mToolbar);
+         getSupportActionBar().setTitle("Create Account");
+         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+        //Firebase Authentication
         mAuth = FirebaseAuth.getInstance();
 
         mDisplayName=(EditText)findViewById(R.id.reg_display_name);
