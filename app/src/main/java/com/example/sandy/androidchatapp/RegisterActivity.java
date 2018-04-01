3adf51251ec79c4258b368ee062d86416aefbd46
Sandeep <sandeeptile@gmail.com>
2018-04-01 17:18:39 +0530

    account settings 1/2

--- app/src/main/java/com/example/sandy/androidchatapp/RegisterActivity.java
+++ app/src/main/java/com/example/sandy/androidchatapp/RegisterActivity.java
@@ -17,6 +17,11 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -30,6 +35,8 @@ public class RegisterActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
 
     private ProgressDialog mRegisterProgress;
+
+    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -84,7 +91,7 @@ public class RegisterActivity extends AppCompatActivity {
         });
     }
 
-    private void register_user(String display_name, String email, String password) {
+    private void register_user(final String display_name, String email, String password) {
 
         mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
             @Override
@@ -92,11 +99,29 @@ public class RegisterActivity extends AppCompatActivity {
 
                 if(task.isSuccessful()){
 
-                    mRegisterProgress.dismiss();
-                    Intent mainIntent=new Intent(RegisterActivity.this,MainActivity.class);
-                    mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                    startActivity(mainIntent);
-                    finish();
+                    FirebaseUser current_user=FirebaseAuth.getInstance().getCurrentUser();
+                    String uid=current_user.getUid();
+
+                    mDatabase= FirebaseDatabase.getInstance().getReference().child("users").child(uid);
+
+                    HashMap<String,String> usermap=new HashMap<>();
+                    usermap.put("name",display_name);
+                    usermap.put("status","Hi there,I'm using Android chatApp");
+                    usermap.put("image","default");
+                    usermap.put("thumb_image","default");
+
+                    mDatabase.setValue(usermap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+
+                            mRegisterProgress.dismiss();
+                            Intent mainIntent=new Intent(RegisterActivity.this,MainActivity.class);
+                            mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                            startActivity(mainIntent);
+                            finish();
+
+                        }
+                    });
 
 
                 }else {
